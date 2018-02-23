fb294c612e1a90a3e646aa641ff89c1f354dc0d6
fet mar <fetmar@gmail.com>
2018-02-23 00:12:33 -0500

    begin refactoring jest tests to mocha+chai+sinon tests

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,12 +1,14 @@
+var expect = require('chai').expect;
+
 const lInfo = require('../src/utils/logger').lInfo;
 describe('logging', ()=>{
   it('should log a message', ()=>{
     const message = lInfo('label','test');
-    expect(message).toMatch('label=test');
+    expect(message).to.match(/label=test/);
   });
   it('should log a message with spaces', ()=>{
     const message = lInfo('label','some label');
-    expect(message).toMatch('label="some label"');
+    expect(message).to.match(/label="some label"/);
   });
 
 });
--- test/makeResponse.test.js
+++ test/makeResponse.test.js
@@ -1,17 +1,19 @@
+const expect = require('chai').expect;
+
 const makeResponse = require('../src/utils/makeResponse');
 const Http = require('http-status-codes');
 
 describe('makeResponse', ()=>{
   it('should make a response object', ()=>
-    expect(makeResponse(Http.OK,{})).toEqual({statusCode:Http.OK,body:{}})
+    expect(makeResponse(Http.OK,{})).to.deep.equal({statusCode:Http.OK,body:{}})
   );
 
   it('should invalidate an invalid body response', ()=>
-    expect(makeResponse(Http.OK,null).statusCode).toBe(Http.INTERNAL_SERVER_ERROR)
+    expect(makeResponse(Http.OK,null).statusCode).to.equal(Http.INTERNAL_SERVER_ERROR)
   );
 
   it('should invalidate an invalid statusCode response', ()=>
-    expect(makeResponse(0,{}).statusCode).toBe(Http.INTERNAL_SERVER_ERROR)
+    expect(makeResponse(0,{}).statusCode).to.equal(Http.INTERNAL_SERVER_ERROR)
   );
 
 });
