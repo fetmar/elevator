a6d0d6ffe71903b563564cc2c654f05ec2c4dc9c
Josh Anderson <fetmar@gmail.com>
2018-03-06 21:56:28 -0500

    never don't not build for non-master

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# These are run after the tests.
+# Errors here won't show as a failed build.
+
+# Only build for master
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 0
+fi
 
 VERSION=$(grep "version" ./package.json | sed -e 's/.*"version".*\([0-9]\.[0-9]\.[0-9]\)".*/\1/')
 BUILD_DATE=$(date --date="Feb 2 2014 13:12:10")
 COMMIT=$(git rev-parse --short HEAD)
+
 docker build -t fetmar/elevator:latest . \
   --build-arg VCS_REF="$COMMIT" \
   --build-arg VERSION="$VERSION" \
