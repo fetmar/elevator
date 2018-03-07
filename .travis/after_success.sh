3ce92a99207f8275c20c75b38053a1318fab519d
Josh Anderson <fetmar@gmail.com>
2018-03-06 22:29:20 -0500

    clean up after_success script

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -3,21 +3,27 @@
 # Errors here won't show as a failed build.
 
 # Only build for master
-#if [ "$TRAVIS_BRANCH" != "master" ]; then
-  #exit 0
-#fi
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 0
+fi
 
+# get variables ready
 VERSION=$(grep "version" ./package.json | sed -e 's/.*"version".*\([0-9]\.[0-9]\.[0-9]\)".*/\1/')
 BUILD_DATE=$(date --date="Feb 2 2014 13:12:10")
 COMMIT=$(git rev-parse --short HEAD)
 
+# docker login
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+# make an image
 docker build -t fetmar/elevator:latest . \
   --build-arg VCS_REF="$COMMIT" \
   --build-arg VERSION="$VERSION" \
   --build-arg BUILD_DATE="$BUILD_DATE"
 
-docker tag fetmar/elevator:latest fetmar/elevator:"$VERSION"
-
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push fetmar/elevator:latest
+# push latest image
 docker push fetmar/elevator:"$VERSION"
+
+# push a versioned image
+docker tag fetmar/elevator:latest fetmar/elevator:"$VERSION"
+docker push fetmar/elevator:latest
